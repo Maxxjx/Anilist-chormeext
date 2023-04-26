ea7efd2f16afd122c0094060fd9eea4c33ae56f4
Maxxjx <maxxssj338@example.com>
2023-04-26 08:25:25 +0530

    changes

--- aniscript.js
+++ aniscript.js
@@ -1,43 +1,41 @@
+async function getData(day="today") {
+   console.log(day);
+  const url = `https://show-air-dates.p.rapidapi.com/${day}`;
+  const options = {
+    method: "GET",
+    headers: {
+      "content-type": "application/octet-stream",
+      "X-RapidAPI-Key": "cf5020d11cmsh5cb0db07a8d40b1p195f8djsnae67d243220d",
+      "X-RapidAPI-Host": "show-air-dates.p.rapidapi.com",
+    },
+  };
 
-// function airtime () {
-//   var airtime = document.getElementById("airing").value;
-//   getData(airtime);
+  const response = await fetch(url, options);
+  const result = await response.json();
+  console.log(result);
 
-// };
+  const tableBody = document.querySelector("#show-table tbody");
+  tableBody.innerHTML = "";
+  result.shows.forEach((show) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>${show.name} (Season ${show.saison})</td>
+      <td>${show.episode}</td>
+    `;
+    tableBody.appendChild(row);
+  });
+}
+getData();
 
-// async function getData(day) {
-//   const url = `https://show-air-dates.p.rapidapi.com/${day ? day:"today"}`;
-//   const options = {
-//     method: "GET",
-//     headers: {
-//       "content-type": "application/octet-stream",
-//       "X-RapidAPI-Key": "cf5020d11cmsh5cb0db07a8d40b1p195f8djsnae67d243220d",
-//       "X-RapidAPI-Host": "show-air-dates.p.rapidapi.com",
-//     },
-//   };
+const refresh= document.getElementById("refresh")
+const selectTime =document.getElementById("airing")
 
-//   const response = await fetch(url, options);
-//   const result = await response.json();
-//   console.log(result);
-//   return result;
-// }
+addEventListener.selectTime("change",airtime());
+addEventListener.refresh("click",airtime());
 
-// async function renderData() {
-//   const data = await getData();
-//   const tableBody = document.querySelector("#show-table tbody");
-//   tableBody.innerHTML = "";
-//   data.shows.forEach((show) => {
-//     const row = document.createElement("tr");
-//     row.innerHTML = `
-//       <td>${show.name} (Season ${show.saison})</td>
-//       <td>${show.episode}</td>
-//     `;
-//     tableBody.appendChild(row);
-//   });
-// }
+function airtime () {
+  var airtime = selectTime.value;
+  getData(airtime);
 
-// function refreshData() {
-//   renderData();
-// }
+};
 
-// renderData();
